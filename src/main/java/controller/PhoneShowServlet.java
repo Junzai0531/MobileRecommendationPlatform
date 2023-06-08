e44c8d753663e2c7b0458824b15217c36d91233a
ruanzijun <1524302342@qq.com>
2023-06-08 19:49:34 +0800

    在PhoneShowServlet中添加了查询全部手机信息的代码

--- src/main/java/controller/PhoneShowServlet.java
+++ src/main/java/controller/PhoneShowServlet.java
@@ -1,15 +1,26 @@
 package controller;
 
+import entity.Phone;
+import service.PhoneService;
+import service.PhoneServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "PhoneShowServlet", urlPatterns = "/show")
 public class PhoneShowServlet extends HomeServlet{
+    private PhoneService phoneService = new PhoneServiceImpl();
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        List<Phone> phones = phoneService.findAllPhones();
+        System.out.println("from FindAllPhonesServlet\n" + phones);
+
         request.getRequestDispatcher("/showPhone.jsp").forward(request, response);
     }
 }
